b25fbe6a1354b8c4123ad9a93cf2e03df6cb7e1e
Edgar Aroutiounian <edgar.factorial@gmail.com>
2016-05-05 03:40:00 -0700

    Tidy up

--- src/runa.ml
+++ src/runa.ml
@@ -1,25 +1,15 @@
-(** Runa is a collection of helper functions for js_of_ocaml
-    programs. *)
-
-(** Easy access of field by name from JS Object *)
 let ( <!> ) obj field = Js.Unsafe.get obj field
 
-(** Wrap your OCaml function to JS with this *)
 let ( !@ ) f = Js.wrap_callback f
 
-(** Alias of Js.Unsafe.inject *)
 let ( !! ) o = Js.Unsafe.inject o
 
-(** Try to get a string out of given Object *)
 let stringify o = Js._JSON##stringify o |> Js.to_string
 
-(** Alias of Js.Unsafe.meth_call*)
 let m = Js.Unsafe.meth_call
 
-(** Combine two JS objects into one *)
 let merge a b : < .. > Js.t = Js.Unsafe.global##.Object##assign a b
 
-(** Create a JS object out of a Jstable *)
 let object_of_table t =
   let js_keys = Jstable.keys t in
   let values =
@@ -32,53 +22,39 @@ let object_of_table t =
   |> Array.of_list
   |> Js.Unsafe.obj
 
-(** Set the global variable DEBUG_VAR to whatever object you
-    provide, useful during debugging *)
 let debug item =
   (Js.Unsafe.global##.DEBUG_VAR := item) |> ignore
 
-(** Log any object to the console *)
 let log l =
   Firebug.console##log l
 
-(** In [set_interval ~every cb], call !{cb} every given seconds.*)
 let set_interval ~every:float (callback: unit -> unit) =
   Dom_html.window##setInterval !@callback float
 
-(** Call your function with the scope of JavaScript's this object,
-    very useful *)
 let with_this f = !!(Js.wrap_meth_callback f)
 
-(** Turn JavaScript string array into OCaml list of string *)
 let to_string_list g =
   g |> Js.str_array |> Js.to_array |> Array.map Js.to_string |> Array.to_list
 
-(** Turn OCaml list of strings into JavaScript string array *)
 let of_string_list g =
   g |> Array.of_list |> Array.map Js.string |> Js.array
 
-(** require for CommonJS environments *)
 let require s =
   Js.Unsafe.fun_call
     (Js.Unsafe.js_expr "require")
     [|Js.Unsafe.inject (Js.string s)|]
 
-(** Parse a string as JSON *)
 let json_parse s = Js._JSON##parse (s |> Js.string)
 
-(** Get all keys of an Object *)
 let keys obj =
   m (Js.Unsafe.variable "Object") "keys" [|obj|]
   |> Js.to_array |> Array.map Js.to_string |> Array.to_list
 
-(** For each item in the object, call function and ignore results *)
 let for_each_iter ~f obj =
   keys obj |> List.iter (fun k -> f (Js.Unsafe.get obj k)
                                   |> ignore)
 
-(** For each item in the object, call function and keep results *)
 let for_each_map ~f obj =
   keys obj |> List.map (fun k -> f k (Js.Unsafe.get obj k))
 
-(** Get the current UNIX time *)
 let time_now () = (new%js Js.date_now)##getTime
--- src/runa.mli
+++ src/runa.mli
@@ -1,24 +1,3 @@
-Runa
-====
-
-Runa is a collection of functions I found myself writing over and over
-in my `js_of_ocaml` programming. It does not assume anything other
-than what is provided by `js_of_ocaml`, aka no assumption of `nodejs`.
-Use this library for yourself as well and make your `OCaml` to
-`JavaScript` coding that much easier; even better contribute some
-snippets as well!
-
-Usage
-=====
-Install by cloning this repo, then in the repo do:
-`opam pin add runa . -y` and then in your `js_of_ocaml` programs
-simply add `runa` to your `-package` declaration, be sure to do
-`-linkpkg` as well for `ocamlfind`.
-
-Interface
-==========
-
-```ocaml
 (** Runa is a collection of helper functions for js_of_ocaml
     programs. *)
 
@@ -80,4 +59,3 @@ val for_each_map : f:(string -> 'a -> 'b) -> Js.Unsafe.any -> 'b list
 
 (** Get the current UNIX time *)
 val time_now : unit -> float
-```
